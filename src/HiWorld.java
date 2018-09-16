574a17a3330ed4c272fa7b43950d22a43b527ce4
HymEric <951523291@qq.com>
2018-09-16 11:49:05 +0800

    add

--- src/HiWorld.java
+++ src/HiWorld.java
@@ -3,5 +3,6 @@ public class HiWorld{
         System.out.println("Hi World from local project push to Github.");
         System.out.println("Second test2!");
         System.out.println("Second test3 提交改动!");
+        System.out.println("Second test3 提交改动2!");
     }
 }
