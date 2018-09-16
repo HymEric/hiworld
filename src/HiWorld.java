6dac85ef1f39dc2e48f93f1066589cfeb1fcefbf
HymEric <951523291@qq.com>
2018-09-16 11:47:49 +0800

    Merge branch 'master' of https://github.com/HymEric/hiworld
    # Please enter a commit message to explain why this merge is necessary,
    # especially if it merges an updated upstream into a topic branch.
    #
    # Lines starting with '#' will be ignored, and an empty message aborts
    # the commit.

--- src/HiWorld.java
+++ src/HiWorld.java
@@ -2,5 +2,6 @@ public class HiWorld{
     public static void main(String args[]) {
         System.out.println("Hi World from local project push to Github.");
         System.out.println("Second test2!");
+        System.out.println("Second test3 提交改动!");
     }
 }
