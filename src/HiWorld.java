97f9ace81ed60429e1556e503a48d4988dca68d2
HymEric <951523291@qq.com>
2018-09-16 11:41:57 +0800

    test2

--- src/HiWorld.java
+++ src/HiWorld.java
@@ -1,6 +1,6 @@
 public class HiWorld{
     public static void main(String args[]) {
         System.out.println("Hi World from local project push to Github.");
-        System.out.println("Second test!");
+        System.out.println("Second test2!");
     }
 }
